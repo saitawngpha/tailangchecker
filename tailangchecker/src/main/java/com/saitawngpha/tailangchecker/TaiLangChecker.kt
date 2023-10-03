8e0a14342a9dc0ffac600cfe35d49133039bb822
Sai Tawng Pha <saitawngpha@gmail.com>
2023-10-03 00:22:21 -0700

    Update some bug

--- tailangchecker/src/main/java/com/saitawngpha/tailangchecker/TaiLangChecker.kt
+++ tailangchecker/src/main/java/com/saitawngpha/tailangchecker/TaiLangChecker.kt
@@ -12,4 +12,10 @@ object TaiLangChecker {
         val regex = "([ၵ|ၶ|င|ၸ|သ|ၺ|တ|ထ|ၼ|ပ|ၽ|ၾ|မ|ယ|ရ|လ|ဝ|ႁ|ဢ](?:ႂ|ွ|ေ|ႄ|ိ|ီ|ဵ|ႅ|ႆ|ၢႆ|ု|ူ|ႃ|ၢ|ျ|ြ|ၵ်|င်|ၺ်|တ်|ၼ်|ပ်|မ်|ဝ်|်|ႇ|ႈ|း|ႉ|ႊ)*|\\s)*"
         return regex.toRegex().matches(text)
     }
+
+    fun isNotTai(text: String): Boolean{
+    val reg = "^(?![ၵ|ၶ|ၸ|ၺ|ၼ|ႁ|ဢ])(?=.*[က-အ](?<foo>ို|ိုး|ယ်|ြင်|ြိုင်|ြို|ြေ|ြ|ာ|ား|ိုင်ပင်|ည်|စ်|ဉ်|ေါ|ေါ်|ဲ|ေး|ှိ|သတိရ|ုန်|န်|ိုင်းပြသူ|ွား)(?!ၵ်|ၺ်|ၼ်)$).*"
+    // println("isnottai: ${reg.toRegex().matches(text)}")
+    return reg.toRegex().matches(text)
+}
 }
